a60cf73f267345b958e7d214e4abad81352017d3
Brent Yorgey <byorgey@cis.upenn.edu>
2012-07-02 01:18:39 +0000

    testing directory with WIP on image uploading

---
+++ test/NewMediaObject.hs
@@ -0,0 +1,32 @@
+import           Network.XmlRpc.Client      (remote)
+import           Network.XmlRpc.Internals   (Value(..), toValue)
+import           Data.Char                  (toLower)
+import           System.FilePath            (takeFileName, takeExtension)
+import qualified Data.ByteString.Char8 as B
+import           Data.Functor               ((<$>))
+
+uploadMediaObject :: FilePath -> IO Value
+uploadMediaObject file = do
+  media <- mkMediaObject file
+  remote "http://mathlesstraveled.com/xmlrpc.php" "metaWeblog.newMediaObject" "default" "byorgey" "a0303017" media
+
+-- Create the required struct representing the image.
+mkMediaObject :: FilePath -> IO Value
+mkMediaObject filePath = do
+  bits <- B.unpack <$> B.readFile filePath
+  return $ ValueStruct
+    [ ("name", toValue fileName)
+    , ("type", toValue fileType)
+    , ("bits", ValueBase64 bits)
+    ]
+  where
+    fileName = takeFileName filePath
+    fileType = case (map toLower . drop 1 . takeExtension) fileName of
+                 "png"  -> "image/png"
+                 "jpg"  -> "image/jpeg"
+                 "jpeg" -> "image/jpeg"
+                 "gif"  -> "image/gif"
+
+main = do
+  v <- uploadMediaObject "images/checkmark_20.png"
+  print v
---
+++ test/TestPost.lhs
@@ -0,0 +1,32 @@
+This is a test.
+
+> x :: Int
+> x = 3
+
+Some more testing. $\pi = \tau/2$.
+
+    foo
+    bar
+
+And with ghci:
+
+    [ghci]
+    (x + 2 :: Int)
+    [1..8]
+
+How about that!  Here's some more ghci:
+
+    [ghci]
+    [1..100]
+
+Inline $\LaTeX$: $\sum_{i=1}^n i^2$ vs. display-style:
+
+$$\sum_{i=1}^n i^2$$
+
+Neato!  And also $latex \pi$.
+
+Here's an image:
+
+![Title](foo.png)
+
+I wonder why deleting a line only goes to the second column.
